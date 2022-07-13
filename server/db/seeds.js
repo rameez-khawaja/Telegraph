088f7302d1da6e88c5b961bc3133a624ace699f7
Rameez <rameez0khawaja@gmail.com>
2022-07-13 16:38:53 +0100

    feedback deletion

--- server/db/seeds.js
+++
@@ -1,8 +0,0 @@
-const db = connect("mongodb://localhost:27017/shelter")
-
-db.posts.insertMany([
-    { title: "post 1", author: "me", content: "string" },
-    { title: "post 2", author: "me", content: "string" },
-    { title: "post 3", author: "me", content: "string" },
-    { title: "post 4", author: "me", content: "string" }
-])
